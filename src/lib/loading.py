e3fac416084e09e156a6fe13962204f986eabe0e
Mark Doughten <doughtem@oregonstate.edu>
2023-07-24 22:16:20 -0400

    loading calendars

--- src/lib/loading.py
+++ src/lib/loading.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import csv
 import sys
+import string
+import re
 
 def load_data(file_path, extension):
     if extension in ['htm', 'html']:
@@ -10,12 +12,12 @@ def load_data(file_path, extension):
             soup = BeautifulSoup(file.read(), 'html.parser')
             text = soup.get_text()
             return text.split('\n')
-    elif extension == 'txt':
+    elif extension in ['txt', 'ics']:
         with open(file_path, 'r', encoding='unicode_escape') as file:
             return file.read()
     elif extension == 'csv':
         with open(file_path, 'r', encoding='unicode_escape') as file:
-            return list(csv.reader(file, delimiter=','))
+            return list(csv.reader(file))
     else:
         return []
 
@@ -24,35 +26,48 @@ def directory(directory_path):
     # initialize a list to hold all the texts
     all_texts = []
     unsupported = set([])
+    unwanted_strings = ['False', 'Normal']
 
     # loop through all files and subdirectories in the directory
     for root, dirs, files in os.walk(directory_path):
         for filename in files:
             file_path = os.path.join(root, filename)
-            extension = file_path.split('.')[-1]
+            extension = file_path.split('.')[-1].lower()
             texts = load_data(file_path, extension)
             if texts:
+                texts = clean_data(texts, unwanted_strings)
                 all_texts.extend(texts)
             else:
+                print(f'cannot open {filename}')
                 unsupported.add(extension)
 
     # all_texts contains the texts from all files
-    print(f'Unsupported file types: {unsupported}')
+    #print(f'Unsupported file types: {unsupported}')
 
     return all_texts
+
+def clean_item(item, unwanted_strings):
+    cleaned_item = ''.join(ch for ch in item if ord(ch) < 128)  # remove non-ASCII
+    cleaned_item = re.sub(r'<a.*?>.*?</a>', '', cleaned_item)  # remove <a> tags
+    for unwanted in unwanted_strings:
+        cleaned_item = cleaned_item.replace(unwanted, '')  # remove unwanted strings
+    return cleaned_item
+
+def clean_data(data, unwanted_strings):
+    new_data = []
+    for item in data:
+        if isinstance(item, list):
+            new_data.append(clean_data(item, unwanted_strings))
+        elif isinstance(item, str) and item != '':  # remove empty strings
+            cleaned_item = clean_item(item, unwanted_strings)
+            if cleaned_item != '':  # remove strings that become empty after cleaning
+                new_data.append(cleaned_item)
+    return new_data
 
 if __name__ == "__main__":
     
     # directory
     path = '../../files'
-    output = directory(path)
-
-    for item in output:
-        if isinstance(item, list):  # Handle the case of CSV reader output
-            for row in item:
-                cleaned_row = [cell.replace(',', '') for cell in row]
-                if cleaned_row:
-                    output_str = ''.join(cleaned_row) + '\n'
-                    sys.stdout.buffer.write(output_str.encode('utf-8'))
-        else:
-            sys.stdout.buffer.write(item.encode('utf-8'))
+    messages = directory(path)
+    print(messages)
+    
