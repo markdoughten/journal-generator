1d30d8fe9eecbea1622bc202b12be220883692f4
Mark Doughten <doughtem@oregonstate.edu>
2023-07-25 21:12:05 -0400

    fixing single ch

--- src/lib/loading.py
+++ src/lib/loading.py
@@ -14,7 +14,7 @@ def load_data(file_path, extension):
             return text.split('\n')
     elif extension in ['txt', 'ics']:
         with open(file_path, 'r', encoding='unicode_escape') as file:
-            return file.read()
+            return file.readlines()
     elif extension == 'csv':
         with open(file_path, 'r', encoding='unicode_escape') as file:
             return list(csv.reader(file))
@@ -26,7 +26,10 @@ def directory(directory_path):
     # initialize a list to hold all the texts
     all_texts = []
     unsupported = set([])
-    unwanted_strings = ['False', 'Normal']
+    unwanted_strings = ['False', 'Normal', 'X-GOOGLE-CALENDAR-CONTENT-DISPLAY:CHIP\n', \
+                        'X-GOOGLE-CALENDAR-CONTENT-ICON:https://calendar.google.com/googlecalendar/i', \
+                        ' mages/cake.gif\n', 'CLASS:PUBLIC\n', \
+                        ' mages/cake.gif', 'X-GOOGLE-CALENDAR-CONTENT-DISPLAY:CHIP']
 
     # loop through all files and subdirectories in the directory
     for root, dirs, files in os.walk(directory_path):
@@ -36,25 +39,27 @@ def directory(directory_path):
             texts = load_data(file_path, extension)
             if texts:
                 texts = clean_data(texts, unwanted_strings)
-                all_texts.extend(texts)
+                all_texts.append(texts)
             else:
-                print(f'cannot open {filename}')
                 unsupported.add(extension)
-
-    # all_texts contains the texts from all files
-    #print(f'Unsupported file types: {unsupported}')
 
     return all_texts
 
 def clean_item(item, unwanted_strings):
+    
     cleaned_item = ''.join(ch for ch in item if ord(ch) < 128)  # remove non-ASCII
     cleaned_item = re.sub(r'<a.*?>.*?</a>', '', cleaned_item)  # remove <a> tags
+    cleaned_item = cleaned_item.replace('\n', '')
+    
     for unwanted in unwanted_strings:
         cleaned_item = cleaned_item.replace(unwanted, '')  # remove unwanted strings
+    
     return cleaned_item
 
 def clean_data(data, unwanted_strings):
+    
     new_data = []
+    
     for item in data:
         if isinstance(item, list):
             new_data.append(clean_data(item, unwanted_strings))
@@ -62,6 +67,7 @@ def clean_data(data, unwanted_strings):
             cleaned_item = clean_item(item, unwanted_strings)
             if cleaned_item != '':  # remove strings that become empty after cleaning
                 new_data.append(cleaned_item)
+    
     return new_data
 
 if __name__ == "__main__":
@@ -70,4 +76,3 @@ if __name__ == "__main__":
     path = '../../files'
     messages = directory(path)
     print(messages)
-    
