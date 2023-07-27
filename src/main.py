87aad440f258f936c6b22270cb5fe79e36d39e31
markdoughten <doughtm1@tcnj.edu>
2023-07-27 19:50:51 -0400

    main and loading messages

--- src/main.py
+++ src/main.py
@@ -1,7 +1,21 @@
 # custom libraries
-from lib import helpers
+from lib import loading, requests, helpers
 
 def main():
+        
+        # load history and train model
+        history = loading.run('../../files')
+        trained_model = requests.load(history)
+        
+        # get questions from users
+        while True:
+            question = input('>')
+            if question.lower('exit')
+                break
+            
+            # print the response 
+            print(trained_model(question))
+    
         return 
 
 if __name__ == '__main__':
