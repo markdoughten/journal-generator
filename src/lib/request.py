1cb5a363bd5d912b935c392703499e7b8ce92e0f
Mark Doughten <doughtem@oregonstate.edu>
2023-08-01 21:48:17 -0400

    journal working

--- src/lib/request.py
+++ src/lib/request.py
@@ -21,4 +21,57 @@ def send(user_input, engine, api_key):
     )
 
     return response.choices[0].text.strip()
+
+def num_tokens(text, model):
+    encoding = tiktoken.encoding_for_model(model)
+    return len(encoding.encode(text))
+
+# search function
+def strings_ranked_by_relatedness(query, df, embedding, relatedness_fn=lambda x, y: 1 - spatial.distance.cosine(x, y), top_n=100):
+    
+    query_embedding_response = openai.Embedding.create(model=embedding, input=query)
+    query_embedding = query_embedding_response["data"][0]["embedding"]
+    strings_and_relatednesses = [(row["text"], relatedness_fn(query_embedding, row["embedding"]))for i, row in df.iterrows()]
+    strings_and_relatednesses.sort(key=lambda x: x[1], reverse=True)
+    strings, relatednesses = zip(*strings_and_relatednesses)
+    
+    return strings[:top_n], relatednesses[:top_n]
+
+def query_message(query, df, model, token_budget, embedding):
+    """Return a message for GPT, with relevant source texts pulled from a dataframe."""
+    
+    strings, relatednesses = strings_ranked_by_relatedness(query, df, embedding)
+    introduction = 'Write a journal entry about a date based on the historical information available'
+    question = f"\n\nDate: {query}"
+    message = introduction
+    
+    for string in strings:
+        next_event = f'\n\nRelevant event:\n"""\n{string}\n"""'
+        if (num_tokens(message + next_event + question, model=model) > token_budget):
+            break
+        else:
+            message += next_event
+    
+    return message + question
+
+def ask(query, df, model, token_budget, embedding, print_message=False):
+    """Answers a query using GPT and a dataframe of relevant texts and embeddings."""
+    
+    message = query_message(query, df, model, token_budget, embedding)
+    
+    if print_message:
+        print(message)
+    
+    messages = [
+        {"role": "system", "content": "You answer questions like journal entries."},
+        {"role": "user", "content": message}]
+
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0
+    )
+    
+    return response["choices"][0]["message"]["content"]
+
     
