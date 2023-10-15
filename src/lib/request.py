29411621e5603771665fcb2b858c26c66cbaaa9e
Mark Doughten <doughtem@oregonstate.edu>
2023-10-15 19:32:25 -0400

    fixing column

--- src/lib/request.py
+++ src/lib/request.py
@@ -29,7 +29,7 @@ def strings_ranked_by_relatedness(query, df, embedding, relatedness_fn=lambda x,
     
     query_embedding_response = openai.Embedding.create(model=embedding, input=query)
     query_embedding = query_embedding_response["data"][0]["embedding"]
-    strings_and_relatednesses = [(row["combined"], relatedness_fn(query_embedding, row["embedding"]))for i, row in df.iterrows()]
+    strings_and_relatednesses = [(row["text"], relatedness_fn(query_embedding, row["embedding"]))for i, row in df.iterrows()]
     strings_and_relatednesses.sort(key=lambda x: x[1], reverse=True)
     strings, relatednesses = zip(*strings_and_relatednesses)
     
